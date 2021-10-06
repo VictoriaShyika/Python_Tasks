c3a641255ee4bfc048f0480b62d14fc773fc807f
VictoriaShyika <victoriashyika@gmail.com>
2021-10-06 23:24:04 +0300

    Revert "Vitalii main commit"
    This reverts commit 5de996d825c363c81e80125049bc92c49b6a0bc5.

--- ascii.py
+++ ascii.py
@@ -4,4 +4,16 @@ def even_or_odd(s):
     for x in s:
         if int(x) % 2 == 0:
             even_arr.append(int(x))
+        else:
+            odd_arr.append(int(x))
+    odd_sum = sum(odd_arr)
+    even_sum = sum(even_arr)
+    if odd_sum > even_sum:
+        return 'Odd is greater than Even'
+    elif even_sum == odd_sum:
+        return 'Even and Odd are the same'
+    else:
+        return 'Even is greater than Odd'
 
+if __name__ == '__main__':
+    print(compress("The bumble bee"))
