a5911072ef8e46b5705d33ea287e9c554b6c9492
VictoriaShyika <victoriashyika@gmail.com>
2021-10-12 12:00:38 +0300

    Changed input data

---
+++ 8_kyu/bin_to_decimal.py
@@ -0,0 +1,17 @@
+def bin_to_decimal(inp):
+    answer = 0
+    counter = 1
+    for x in reversed(inp):
+        answer += int(x) * counter
+        counter *= 2
+    return answer
+
+
+if __name__ == '__main__':
+    assert bin_to_decimal("0") == 0
+    assert bin_to_decimal("1") == 1
+    assert bin_to_decimal("10") == 2
+    assert bin_to_decimal("11") == 3
+    assert bin_to_decimal("101010") == 42
+    assert bin_to_decimal("1001001") == 73
+
