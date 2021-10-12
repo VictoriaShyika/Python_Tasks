314fca991d193f244720cf2489fa19b369342d66
VictoriaShyika <victoriashyika@gmail.com>
2021-10-12 13:50:56 +0300

    Added even_or_odd

---
+++ 8_kyu/even_or_odd.py
@@ -0,0 +1,19 @@
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+if __name__ == '__main__':
+    assert even_or_odd(2) == "Even"
+    assert even_or_odd(1) == "Odd"
+    assert even_or_odd(0) == "Even"
+    assert even_or_odd(1545452) == "Even"
+    assert even_or_odd(7) == "Odd"
+    assert even_or_odd(78) == "Even"
+    assert even_or_odd(17) == "Odd"
+    assert even_or_odd(74156741) == "Odd"
+    assert even_or_odd(100000) == "Even"
+    assert even_or_odd(-123) == "Odd"
+    assert even_or_odd(-456) == "Even"
