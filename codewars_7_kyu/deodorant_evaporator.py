2959972aace7ba707bda126c0a162ef00cdc28fe
VictoriaShyika <victoriashyika@gmail.com>
2021-10-14 16:55:53 +0300

    Added deodorant_evaporator

---
+++ codewars_7_kyu/deodorant_evaporator.py
@@ -0,0 +1,12 @@
+def evaporator(content, evap_per_day, threshold):
+    days_counter = 0
+    content = 100
+    while content >= threshold:
+        content *= (1 - evap_per_day / 100)
+        days_counter += 1
+        pass
+    return days_counter
+
+
+if __name__ == '__main__':
+    assert evaporator(10, 10, 10) == 22
